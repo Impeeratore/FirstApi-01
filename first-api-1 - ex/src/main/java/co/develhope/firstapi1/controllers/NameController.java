675caf0bca64e10a85b41442de54ccefebda99fc
Matteo De Lorenzo <delorenzomatteo9@gmail.com>
2022-05-23 09:11:43 +0100

    API01 Commit

---
+++ first-api-1 - ex/src/main/java/co/develhope/firstapi1/controllers/NameController.java
@@ -0,0 +1,24 @@
+package co.develhope.firstapi1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/name")
+public class NameController {
+
+    private String myName = "Francis";
+
+    @GetMapping("")
+    public String getName(){
+        return "Hello, my name is " + myName;
+    }
+
+    @PostMapping("")
+    public String postReversedName(){
+        return new StringBuilder().append(myName).reverse().toString();
+    }
+
+}
